e36a8e213f442d8cd0fa3ae1b1b214e8e47c5e9b
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2017-03-10 01:37:07 -0600

    Basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule,
